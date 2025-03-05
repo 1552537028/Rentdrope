bd026bbc57719d9b9ac3a1496d5822bbd908bc62
Jay <112299655+1552537028@users.noreply.github.com>
2025-03-05 19:44:06 +0530

    Update Profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,74 +1,3 @@
-/*import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-
-const Profile = () => {
-  const [user, setUser] = useState(null);
-
-  const navigate = useNavigate();
-  useEffect(() => {
-    const fetchUser = async () => {
-      const token = localStorage.getItem('token');
-      if (!token) {
-        return;
-      }
-
-      try {
-        const response = await axios.get('http://localhost:5000/api/auth/me', {
-          headers: { Authorization: `Bearer ${token}` },
-        });
-        setUser(response.data);
-      } catch (error) {
-        console.error('Error fetching user:', error.response ? error.response.data : error.message);
-      }
-    };
-
-    fetchUser();
-  }, []);
-
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-   navigate('/')
-  };
-  const handlechange = ()=>{
-    navigate('/login')
-  }
-
-  if (!user){
-    return (<div class="flex items-center justify-center h-screen">
-  <div class="text-center">
-    <p class="mb-4 text-lg">Sorry,Your login has been expired,please login again</p>
-    <button 
-      onClick={handlechange} 
-      className="bg-green-500 text-white py-2 px-4 rounded shadow hover:bg-green-600 transition duration-200"
-    >Login
-    </button>
-  </div>
-</div>
-  )
-  }else{
-  return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="bg-white p-6 rounded shadow-md w-96">
-        <h2 className="mb-4 text-lg font-semibold">Your Profile</h2>
-        <p><strong>Name:</strong> {user.name}</p>
-        <p><strong>Email:</strong> {user.email}</p>
-        <p><strong>Phone:</strong> {user.phone}</p>
-        <button
-          onClick={handleLogout}
-          className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
-        >
-          Logout
-        </button>
-      </div>
-    </div>
-  );
- }
-};
-
-export default Profile;
-*/
-
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -89,7 +18,7 @@ const Profile = () => {
       }
 
       try {
-        const response = await axios.get('http://localhost:5000/api/auth/me', {
+        const response = await axios.get('https://rentdrope-1.onrender.com/api/auth/me', {
           headers: { Authorization: `Bearer ${token}` },
         });
         setUser(response.data);
