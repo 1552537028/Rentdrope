1dd3d6a4279f235d4e6c9c3495f5337700dd48c5
Jay <112299655+1552537028@users.noreply.github.com>
2025-03-05 19:40:46 +0530

    Update Cart.jsx

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -42,7 +42,7 @@ const Cart = () => {
                     <Link to={`/products/${item.id}`} className="text-xl">
                       <div className='flex flex-row'>
                         <img
-                          src={`http://localhost:5000/${item.images[0]}`}
+                          src={`https://rentdrope-1.onrender.com/${item.images[0]}`}
                           alt={item.title}
                           className="w-16 h-16 mr-4"
                         />
@@ -84,7 +84,7 @@ const Cart = () => {
                   <Link to={`/products/${item.id}`} className="text-xl">
                     <div className='flex flex-row'>
                       <img
-                        src={`http://localhost:5000/${item.images[0]}`}
+                        src={`https://rentdrope-1.onrender.com/${item.images[0]}`}
                         alt={item.title}
                         className="w-16 h-16 mr-4"
                       />
