623b45a3dce44c95e2e6f549c0ab54ae16ac4d01
Jay <112299655+1552537028@users.noreply.github.com>
2025-03-08 08:22:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,44 +11,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: "https://rentdrope.onrender.com" }));
-app.use(bodyParser.json());
-// Serve static files (images) from the 'uploads' directory
-const uploadsDir = path.join(__dirname, 'uploads');
-app.use('/uploads', express.static(uploadsDir));  // This allows access to uploaded images
-
-// MongoDB connection
-mongoose.connect('mongodb+srv://jayanth:jayanth@cluster0.ubyck.mongodb.net/rentdrope', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('MongoDB connected');
-}).catch(err => {
-  console.error('MongoDB connection error:', err);
-});
-
-// Routes
-app.use('/api/products', productRoutes);
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-
-/*const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const productRoutes = require('./routes/products');
-require('dotenv').config();
-
-// Initialize the Express app
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Middleware
 app.use(cors({origin:"https://rentdrope.onrender.com"}));
 app.use(bodyParser.json());
 // Serve static files (images) from the 'uploads' directory
@@ -72,5 +34,5 @@ app.use('/api/products', productRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-*/
+
 
